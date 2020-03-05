05d7726e69f533da045ff554f70bc29e9b894f2d
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-05 14:36:19 -0800

    sumAverage fx with correct rounding, not truncation

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,14 +1,31 @@
 using System;
 using System.Collections.Generic;
 
-namespace RPS.Models
+public class Program
 {
-  public class Program
-  {
 
-    public static void Main()
+  public static double sumAverage(int[] arr1, int[] arr2)
+  {
+    int sum = 0;
+    int totalLength = arr1.Length + arr2.Length;
+    foreach (int num in arr1)
     {
-      Console.WriteLine("hello");
+      sum += num;
     }
+    foreach (int num in arr2)
+    {
+      sum += num;
+    }
+    double rawAverage = (double) sum / totalLength;
+    double average = Math.Round(rawAverage, MidpointRounding.AwayFromZero);
+    return average;
+  }
+
+  public static void Main()
+  {
+    int[] arr1 = { 3 };
+    int[] arr2 = { 2 };
+    double result = sumAverage(arr1, arr2);
+    Console.WriteLine(result);
   }
 }
